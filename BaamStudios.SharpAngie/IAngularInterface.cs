9f52ab7fa01eca75c1eaccb43e37ba222bb78e91
chrwoizi <christian@woizischke.de>
2014-05-24 04:03:15 +0200

    Update IAngularInterface.cs

--- BaamStudios.SharpAngie/IAngularInterface.cs
+++ BaamStudios.SharpAngie/IAngularInterface.cs
@@ -5,14 +5,16 @@ namespace BaamStudios.SharpAngie
     public interface IAngularInterface
     {
         /// <summary>
-        /// Sets the entire model in javascript.
+        /// Sets the entire model in javascript. The implementation should call window.setModel(modelJson).
         /// </summary>
+        /// <param name="clientId">user-defined value from the Bridge.Initialize method.</param>
         /// <param name="modelJson"></param>
         void SetModel(string clientId, string modelJson);
 
         /// <summary>
-        /// Sets a (new) property in javascript.
+        /// Sets a (new) property in javascript. The implementation should call window.setModelProperty(propertyPath, valueJson).
         /// </summary>
+        /// <param name="clientId">user-defined value from the Bridge.Initialize method.</param>
         /// <param name="propertyPath"></param>
         /// <param name="valueJson"></param>
         void SetModelProperty(string clientId, string propertyPath, string valueJson);
